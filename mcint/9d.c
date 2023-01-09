98a95df3c09aa5356ae11d25c8b638018fa1ee1d
lyazj <seeson@pku.edu.cn>
2023-01-09 17:40:26 +0800

    Add 9d.

---
+++ mcint/9d.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define NTRAIL 1000
+
+static void do_trail(int total);
+static double do_int(int total);
+
+int main(void)
+{
+  srand48(0);
+  do_trail(100);
+  do_trail(1000);
+  do_trail(10000);
+  do_trail(100000);
+  do_trail(1000000);
+  do_trail(10000000);
+  return 0;
+}
+
+void do_trail(int total)
+{
+  int t;
+
+  fprintf(stderr, "%d points:\n", total);
+  printf("%d:\n", total);
+  for(t = 0; t < NTRAIL; ++t)
+  {
+    printf("%+25.17e\n", do_int(total));
+    if((t + 1) % (NTRAIL / 100) == 0)
+      fprintf(stderr, "%4d / %d\n", t + 1, NTRAIL);
+  }
+  fprintf(stderr, "\n");
+  printf("\n");
+}
+
+double do_int(int total)
+{
+  double x, y = 0.0, w = 1.0 / total;
+  int p, i;
+
+  for(p = 0; p < total; ++p)
+  {
+    x = 0.0;
+    for(i = 0; i < 9; ++i)
+      x += pow(drand48() - 0.5, 2);
+    y += w * exp(-100.0 * x);
+  }
+  return y;
+}
